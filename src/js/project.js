f16da7f1935605c4114307e5474619b20fad7da4
Ignacio <jakrich@cifpfbmoll.eu>
2021-11-12 18:05:04 +0100

    funcionamiento del boton Go Top correcto

--- src/js/project.js
+++ src/js/project.js
@@ -1,7 +1,35 @@
+//button go to top
+window.onscroll = function(){
+    console.log(document.documentElement.scrollTop);
+    if(document.documentElement.scrollTop > 100) {
+      document.querySelector('.goBackContainer').classList.add('display');
+       
+    }
+    else{
+      document.querySelector('.goBackContainer').classList.remove('display');
+    }
+  }
+
+function goBack(){ 
+    const buttons = document.querySelectorAll('.goBackContainer');
+
+    buttons.forEach(button => {
+      button.addEventListener('click', function(){
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+        });
+      });
+    });
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+  goBack();
+});
 
 //Slide front
 //https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_auto
-var slideIndex = 1;
+ var slideIndex = 1;
 showDivs(slideIndex);
 document.querySelector(".prev").addEventListener("click", function (){
     let n = -1;
@@ -27,4 +55,4 @@ function showDivs(n) {
         elementsByClassName[i].style.display = "none";
     }
     elementsByClassName[slideIndex-1].style.display = "block";
-}
+} 
