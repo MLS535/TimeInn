6722f1ab11bf2dbb3bd2c83f44c7f5ccb8345207
MLS535 <74008091+MLS535@users.noreply.github.com>
2021-11-26 19:34:22 +0100

    Boton de editar funcionando correctamente

--- src/js/renderEvents.js
+++ src/js/renderEvents.js
@@ -4,6 +4,7 @@ import {events, news} from "./data.js";
 
 
 const porfolioEvents = document.querySelector('#portfolio-events');
+const btnEdit = document.getElementById("boton_editar");
 
 const eventos = [...events];
 
@@ -44,25 +45,34 @@ const function_events = function (events){
 
 const removeEvents =function (){
     porfolioEvents.addEventListener('click', evt => {
-        console.log(evt.target);
         const papelera = evt.target.closest('.delete');
-        console.log(papelera);
         if ( !papelera ) return
         papelera.parentElement.parentElement.remove()
     })
 }
-
 removeEvents();
+
+let new_event = document.querySelector(".button_event");
+
+
+new_event.addEventListener("click", function (){
+    btnEdit.style.display ="none";
+    document.querySelector(".submit").style.display ="block";
+})
 
 
 var formOverlay = document.getElementById("formOverlay");
 
-formOverlay.addEventListener("submit", function(evt) {
+formOverlay.addEventListener("click", function(evt) {
     evt.preventDefault();
+    let submit = evt.target.closest(".submit");
+        console.log (submit);
+    if ( !submit ) return
+        console.log (submit);
+    document.querySelector(".submit").style.display ="block";
     // const submit = evt.target.closest('.submit');
     // if ( !submit ) return
     //     window.location.replace("eventPage.html#");
-
      let title = document.getElementById("title");
      let location = document.getElementById("location");
     let publication_date = document.getElementById("date_form");
@@ -79,13 +89,66 @@ formOverlay.addEventListener("submit", function(evt) {
     //hacer una funcion de la clase en concreto de donde está situado la funcion de editar.
    porfolioEvents.insertAdjacentHTML("afterbegin", function_events([eventAdd]).join("\n"));
 
-        title.value="";
-        location.value= "";
-        publication_date.value= "";
-        time.value = "";
-        price.value="";
-        description.value= "";
-        imgUrl.value = "";
+        // title.value="";
+        // location.value= "";
+        // publication_date.value= "";
+        // time.value = "";
+        // price.value="";
+        // description.value= "";
+        // imgUrl.value = "";
 
     }
     );
+
+
+
+
+const editEvents =function (){
+    porfolioEvents.addEventListener('click', evt => {
+
+        const editar = evt.target.closest('.edit');
+        if ( !editar ) return
+        const editParent = editar.parentElement.parentElement
+        btnEdit.style.display ="block";
+        document.querySelector(".submit").style.display ="none";
+        window.location.replace("eventPage.html#popup1")
+        btnEdit.addEventListener('click', function()
+        {
+            let title = document.getElementById("title");
+            let location = document.getElementById("location");
+            let publication_date = document.getElementById("date_form");
+            let time = document.getElementById("time_form");
+            let price = document.getElementById("price");
+            let description = document.getElementById("description");
+            let imgUrl = document.getElementById("imgUrl");
+
+
+            // const eventosEditados = {title: title.value,description: description.value, publication_date: publication_date.value, imgUrl: imgUrl.value
+            //     , time: time.value, price: price.value, location: location.value};
+
+
+            // function_events(eventosEditados).join("\n");
+            const markup = `<img class="image" src="${imgUrl.value}" alt="${title.value}"/>
+            <div class="content-overlay">
+                <div class="title">${title.value}</div>
+                <div class="event_favicon">
+                    <div class="date-destacado"><i class="far fa-calendar-alt"></i> ${publication_date.value}
+                    </div>
+                    <div><i class="far fa-clock"></i> Hora: ${time.value}</div>
+                    <div><i class="fas fa-hand-holding-usd"></i> Precio: ${price.value}</div>
+                    <div><i class="fas fa-map-marked-alt"></i> ${location.value}</div>
+                </div>
+          
+                <div class="text">${description.value}</div>
+            </div>
+            <div class="buttons">
+                <div class="edit"><i class="fas fa-pencil-alt"></i></div>
+                <div class="delete"><i class="fas fa-trash-alt"></i></div>
+            </div>`
+            editParent.innerHTML = ''
+        editParent.insertAdjacentHTML('afterbegin', markup);
+            console.log (markup)
+        })
+    })
+}
+editEvents();
