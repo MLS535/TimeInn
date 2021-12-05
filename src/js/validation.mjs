9c38d7b90275dbbe81cf649d68dc1a860d5788fd
Ignacio-Akrich <74008213+Ignacio-Akrich@users.noreply.github.com>
2021-12-05 19:40:59 +0100

    validaciones del password y el mail hechos

--- src/js/validation.mjs
+++ src/js/validation.mjs
@@ -30,7 +30,26 @@ TODO validar el email
         - tipo: Solo pudeden ser com, net o gov
         - Solo puede haber una @ y un punto
 */
-
+function validarEmail(email) {
+    //la primera letra en mayúscula 
+    if (email.charAt(0) != email.charAt(0).toUpperCase()) return "El email debe empezar con una letra mayúscula";
+    //el resto en minúscula
+    if (email.substring(1).toLowerCase() != email.substring(1)) return "El email debe empezar con una letra mayúscula";
+    
+    //Solo pueden haber una @ y un punto
+    if (email.indexOf("@") == -1) return "El email debe tener una @";
+    if (email.indexOf(".") == -1) return "El email debe tener un punto";
+    if (email.indexOf("@") > email.indexOf(".")) return "El email debe tener una @ antes del punto";
+    // entr el @ y el punto, debe haber entre 5 y 10 minusculas
+    if (email.substring(email.indexOf("@")+1, email.indexOf(".")).length < 5 || email.substring(email.indexOf("@")+1, email.indexOf(".")).length > 10) return "entr el @ y el punto, debe haber entre 5 y 10 minusculas";
+    //despues del punto, solo puede haber com, net o gov
+    if (email.substring(email.indexOf(".")+1) != "com" && email.substring(email.indexOf(".")+1) != "net" && email.substring(email.indexOf(".")+1) != "gov") return "despues del punto, solo puede haber com, net o gov";
+    //Validar que el email no exista en la tabla de usuarios
+    let dataEmail = {
+        email: email
+    };
+    return dataEmail;   
+}
 
 /* 
 TODO validar el password
@@ -40,5 +59,64 @@ TODO validar el password
         - Longitud mínima de siete caracteres
         - Incluir letras mayúsculas y minúsculas
         - Incluir números
-        - Incluir caracteres especiales: ñ, $, % ë (esta parte es opcional, pero es una mejora)
+        - Incluir caracteres espespeciales (por ejemplo, !, $, %, &, etc.)
 */
+function validarPassword(password){
+    //Longitud mínima de siete caracteres
+    if (password.length < 6) return "La contraseña debe tener al menos 7 caracteres";
+    //Incluir letras mayúsculas y minúsculas
+    if (!password.match(/[a-z]/)) return "La contraseña debe incluir al menos una letra minúscula";
+    if (!password.match(/[A-Z]/)) return "La contraseña debe incluir al menos una letra mayúscula";
+    //Incluir números
+    if (!password.match(/\d/)) return "La contraseña debe incluir al menos un número";
+    //Incluir caracteres espespeciales (por ejemplo, !, $, %, &, etc.)
+    if (!password.match(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/)) return "La contraseña debe incluir al menos un caracter especial";
+    
+    let dataPassword = {
+        password: password
+    };
+    return dataPassword;
+    
+}
+
+//validacion del submit   
+function validations() { 
+        let email = document.getElementById('email').value;
+        let password = document.getElementById('passwd1').value;
+        let confirmPassword = document.getElementById('passwd2').value;
+
+        validarEmailSalir();
+        validarPasswordSalir()
+        if (password != confirmPassword) {
+            document.getElementById("error").innerHTML = "Las contraseñas no coinciden"; 
+            return false; 
+        } else {
+            document.getElementById("error").innerHTML = "";
+            return true;
+    }
+    };
+    
+// validacion del email al salir del campo
+function validarEmailSalir() {
+    let email = document.getElementById('email').value;
+    let resultEmail = validarEmail(email);
+    if (typeof resultEmail == "string") {
+        document.getElementById("error").innerHTML = "Error Email: "+ resultEmail;
+        return false;
+    } else {
+        document.getElementById("error").innerHTML = "";
+    }
+}
+
+// validacion del password al salir del campo
+function validarPasswordSalir() {
+    let password = document.getElementById('passwd1').value;
+    let resultPassword = validarPassword(password);
+    if (typeof resultPassword == "string") {
+        document.getElementById("error").innerHTML = "Error Contraseña: "+ resultPassword; 
+        return false;
+    } else {
+        document.getElementById("error").innerHTML = "";
+    }
+    
+}
