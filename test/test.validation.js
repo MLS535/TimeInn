4f540cb187d05b749a603840107ac3c3507969f8
MLS535 <74008091+MLS535@users.noreply.github.com>
2021-12-13 18:57:47 +0100

    Tests realizados y pasando correctamente

--- test/test.validation.js
+++ test/test.validation.js
@@ -1,7 +1,7 @@
 import assert from 'assert';
 import {validarNombre, validarEmail, validarPassword} from '../src/js/validation.mjs';
 
-//TODO funciones de validacion con mocha: 3 describe
+//TODO funciones de validacion con mocha: 2 describe y 3 it functions
 var testObj;
   describe('validar elNombre', function (){
       beforeEach( function (){
@@ -18,17 +18,20 @@ var testObj;
       it ( 'validar email', function () {
           assert.equal(validarEmail('mAiteladAria@gmail.com', testObj),'El email solo puede tener la primera letra en mayúscula' );
           assert.equal(validarEmail('Ignacio', testObj),'El email debe tener una @' );
-          assert.equal(validarEmail('Ignacio', testObj),'El email debe tener un punto' );
-          assert.equal(validarEmail('Ignacio.', testObj),'El email debe tener una @ antes del punto' );
-          assert.equal(validarEmail('Ignacio@.com', testObj),'entre el @ y el punto, debe haber entre 5 y 10 minusculas' );
+          assert.equal(validarEmail('Ignacio@', testObj),'El email debe tener un punto' );
+          assert.equal(validarEmail('Ignacio.@', testObj),'El email debe tener una @ antes del punto' );
+          assert.equal(validarEmail('Ignacio@gm.com', testObj),'entre el @ y el punto, debe haber entre 5 y 10 minusculas' );
           assert.equal(validarEmail('Maiteladaria@gmail.com', testObj),'Email ya existe' );
       } );
-      it ( 'validar password', function () {
-          assert.equal(validarPassword('Igna'),'La contraseña debe tener al menos 7 caracteres' );
-          assert.equal(validarPassword('IGNACIO'),'La contraseña debe incluir al menos una letra minúscula' );
-          assert.equal(validarPassword('ignacio'),'La contraseña debe incluir al menos una letra mayúscula' );
-          assert.equal(validarPassword('ignacio'),'La contraseña debe incluir al menos un número' );
-          assert.equal(validarPassword('Ignacio1'),'La contraseña debe incluir al menos un caracter especial' );
-      } );
+
   })
-//TODO funciones de validacion con mocha: 2 it function
+
+describe('validar pass', function (){
+    it ( 'validar password', function () {
+        assert.equal(validarPassword('Igna'),'La contraseña debe tener al menos 7 caracteres' );
+        assert.equal(validarPassword('IGNACIO'),'La contraseña debe incluir al menos una letra minúscula' );
+        assert.equal(validarPassword('ignacio'),'La contraseña debe incluir al menos una letra mayúscula' );
+        assert.equal(validarPassword('Ignacio'),'La contraseña debe incluir al menos un número' );
+        assert.equal(validarPassword('Ignacio1'),'La contraseña debe incluir al menos un caracter especial' );
+    } );
+})
